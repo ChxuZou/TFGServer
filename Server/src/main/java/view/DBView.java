578b5affa6cd0b691a4090f73cbdcd71ee4100d8
Chen <chenxuan.zou2015@gmail.com>
2024-06-14 00:36:26 +0200

    Posibilidad de cerrar la ventana de busqueda sin cerrar el servidor

--- Server/src/main/java/view/DBView.java
+++ Server/src/main/java/view/DBView.java
@@ -39,7 +39,7 @@ public class DBView extends JFrame {
 
 	public DBView(DBViewController controller) {
 		this.controller = controller;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 578, 410);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
